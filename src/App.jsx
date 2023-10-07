9007df89568394adc452d67ab53fb176bfca0451
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-07 18:47:13 +0000

    added the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import destopBg from "./assets/bg-intro-desktop.png"
+import mobileBg from "./assets/bg-intro-mobile.png"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={`${screenWidth <= 640? "bg-[url(./assets/bg-intro-mobile.png)]":"bg-[url(./assets/bg-intro-desktop.png)]"}`}>
+        <div>
+          <h1>Learn to code by watching others</h1>
+          <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <div>Try it free 7 days <span>then $20/mo. thereafter</span></div>
+          <form 
+            className='flex flex-col justify-center items-center'
+            >
+            <input
+              placeholder="First Name" 
+              name="firstName"
+              type="text" 
+            />
+            <input 
+              placeholder="Last Name" 
+              name="lastName"
+              type="text"
+            />
+            <input 
+              placeholder="Email Address" 
+              name="email"
+              type="email" 
+            />
+            <input 
+              placeholder="Password"
+              name="password"
+              type="password" 
+            />
+            <button>CLAIM YOUR FREE TRIAL</button>
+          </form>
+          <p>By clicking the button,you are agreeing to our <span className='text-red-400'>Terms and Services</span></p>
+        </div>
+    </div>
   )
 }
 
