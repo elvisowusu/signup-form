ad1ac85621cfaaf2b9ae98f74caf9d69f0700995
elvisowusu <owusuelvisgyasi@gmail.com>
2023-10-07 23:24:34 +0000

    customised color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'blue':'hsl(248, 32%, 49%)',
+        'reddish':'hsl(0, 100%, 74%)',
+        'darkBlue':'hsl(249, 10%, 26%)', 
+        'greenish':'hsl(154, 59%, 51%)',
+        'grayishBlue': 'hsl(246, 25%, 77%)',
+      }
+    },
   },
   plugins: [],
 }
